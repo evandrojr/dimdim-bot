5cddf07942853b981c636acf0ee7d90a22637040
Evandro Junior <evandrojr@gmail.com>
2017-12-24 02:58:34 -0300

    d

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 require("dotenv-safe").load()
 const MercadoBitcoin = require("./api").MercadoBitcoin
 const MercadoBitcoinTrade = require("./api").MercadoBitcoinTrade
-var infoApi = new MercadoBitcoin({ currency: 'BTC' })
-var tradeApi = new MercadoBitcoinTrade({ 
-    currency: 'BTC', 
-    key: process.env.KEY, 
-    secret: process.env.SECRET, 
-    pin: process.env.PIN 
+var infoApi = new MercadoBitcoin({ currency: 'LTC' })
+var tradeApi = new MercadoBitcoinTrade({
+    currency: 'LTC',
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    pin: process.env.PIN
 })
 
 function getQuantity(coin, price, isBuy, callback){
@@ -16,34 +16,50 @@ function getQuantity(coin, price, isBuy, callback){
 
     tradeApi.getAccountInfo((response_data) => {
         var balance = parseFloat(response_data.balance[coin].available).toFixed(5)
-		balance = parseFloat(balance)
-        if(isBuy && balance < 50) return console.log('Sem saldo disponível para comprar!')
+        balance = parseFloat(balance)
+        if(coin != 'ltc'){
+            if(isBuy && balance < 50) return console.log('Sem saldo disponível para comprar!')
+        }
         console.log(`Saldo disponível de ${coin}: ${balance}`)
-        
+
         if(isBuy) balance = parseFloat((balance / price).toFixed(5))
         callback(parseFloat(balance) - 0.00001)//tira a diferença que se ganha no arredondamento
-    }, 
+    },
     (data) => console.log(data))
 }
 
-setInterval(() => 
+setInterval(() =>
    infoApi.ticker((tick) => {
        console.log(tick)
-       if(tick.sell <= 50000){
-           getQuantity('BRL', tick.sell, true, (qty) => {
-                tradeApi.placeBuyOrder(qty, tick.sell, 
-                    (data) => {
-                        console.log('Ordem de compra inserida no livro. ' + data)
-                        //operando em STOP
-                        tradeApi.placeSellOrder(data.quantity, tick.sell * parseFloat(process.env.PROFITABILITY), 
-                            (data) => console.log('Ordem de venda inserida no livro. ' + data),
-                            (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data))
-                    },
-                    (data) => console.log('Erro ao inserir ordem de compra no livro. ' + data))
-           })
-       }
-       else
-            console.log('Ainda muito alto, vamos esperar pra comprar depois.')
+
+       getQuantity('LTC', tick.sell, false, (qty) => {
+
+                    console.log(qty)
+
+               })
+
+
+
+      // if(tick.sell <= 50000){
+    //        getQuantity('BRL', tick.sell, true, (qty) => {
+    //             tradeApi.placeBuyOrder(qty, tick.sell,
+    //                 (data) => {
+    //                     console.log('Ordem de compra inserida no livro. ' + data)
+    //                     //operando em STOP
+    //                     tradeApi.placeSellOrder(data.quantity, tick.sell * parseFloat(process.env.PROFITABILITY),
+    //                         (data) => console.log('Ordem de venda inserida no livro. ' + data),
+    //                         (data) => console.log('Erro ao inserir ordem de venda no livro. ' + data))
+    //                 },
+    //                 (data) => console.log('Erro ao inserir ordem de compra no livro. ' + data))
+    //        })
+    //    }
+    //    else
+    //         console.log('Ainda muito alto, vamos esperar pra comprar depois.')
+
+
+
+
+
    }),
    process.env.CRAWLER_INTERVAL
 )
